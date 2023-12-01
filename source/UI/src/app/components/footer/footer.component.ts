d19cc0543b485ea673134ed0569736dee77dac4b
Kamil Nowak <kamil.nowak@mail.com>
2023-12-01 19:58:35 +0100

    page component

--- source/UI/src/app/components/footer/footer.component.ts
+++ source/UI/src/app/components/footer/footer.component.ts
@@ -1,35 +1,9 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements AfterViewInit {
-  @ViewChild('footer') footer?: ElementRef<HTMLElement>
-  constructor(private route: ActivatedRoute) {
-  }
-
-  ngOnInit() {
-    this.route.queryParams.subscribe(() => {
-      const isOverflowing = document.body.scrollWidth > document.body.clientWidth;
-
-      if (!isOverflowing) {
-        this.footer?.nativeElement.classList.add('show');
-      } else {
-        this.footer?.nativeElement.classList.remove('show');
-      }
-    });
-  }
-
-  ngAfterViewInit(): void {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY >= 100) {
-        this.footer?.nativeElement.classList.add('show');
-      } else {
-        this.footer?.nativeElement.classList.remove('show');
-      }
-    });
-  }
+export class FooterComponent {
 }
